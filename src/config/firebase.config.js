c7f5a6de9e138f2a861fbbaf63d7bf3dce869528
Deepaktyagi <tdtyagideepak@gmail.com>
2023-06-10 16:35:22 +0530

    firebase config

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -5,4 +5,6 @@ export default {
   projectId: "darshanapp-875ca",
   storageBucket: "darshanapp-875ca.appspot.com",
   messagingSenderId: "787195111757",
+  appId: "1:787195111757:web:a454224eeba0033358ef04",
+  measurementId: "G-Z789Y0SL68"
 };
