957564034881d2594321d890a60bfa1e4719c8a2
Deepaktyagi <tdtyagideepak@gmail.com>
2023-06-10 16:41:10 +0530

    firebase config

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: "AIzaSyAXCfUBuC6eLFdsJcpf-sZJImjEb8mbZPU",
-  authDomain: "darshanapp-875ca.firebaseapp.com",
-  databaseURL: "https://darshanapp-875ca-default-rtdb.firebaseio.com",
-  projectId: "darshanapp-875ca",
-  storageBucket: "darshanapp-875ca.appspot.com",
-  messagingSenderId: "787195111757",
-  appId: "1:787195111757:web:a454224eeba0033358ef04",
-  measurementId: "G-Z789Y0SL68"
+  apiKey: "AIzaSyD0XaIyQFI4TiwJNHefTbSAB7_dfHyCpE4",
+  authDomain: "traffic-122d3.firebaseapp.com",
+  databaseURL: "https://traffic-122d3-default-rtdb.firebaseio.com",
+  projectId: "traffic-122d3",
+  storageBucket: "traffic-122d3.appspot.com",
+  messagingSenderId: "656817207205",
+  appId: "1:656817207205:web:c29bd2db36eea72edada4c",
+  measurementId: "G-7PVJQRMWDR"
 };
